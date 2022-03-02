e899be104b1b4c1b5c66ca01089243d5714904d0
TerminatorxxD <tuanlinhdoan2@gmail.com>
2022-03-02 20:48:50 +0700

    Show all students

---
+++ src/Controller/StudentController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Student;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StudentController extends AbstractController
+{
+    /**
+     * @Route("/student", name="student_list")
+     */
+    public function showAllStudent()
+    {
+        $em = $this->getDoctrine()->getRepository(Student::class);
+        $students= $em->findAll();
+        return $this->render('student/index.html.twig', array('students' => $students));
+    }
+}
