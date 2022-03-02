d309cfef47ddce34ce68940416af58dbad9cd501
TerminatorxxD <tuanlinhdoan2@gmail.com>
2022-03-02 21:03:30 +0700

    Add a new Student

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Student;
+use App\Form\StudentType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,4 +21,27 @@ class StudentController extends AbstractController
         $students= $em->findAll();
         return $this->render('student/index.html.twig', array('students' => $students));
     }
+
+    /**
+     * @Route("/student/create", name="student_create", methods={"GET","POST"})
+     */
+    public function new(Request $request, EntityManagerInterface $entityManager)
+    {
+        $student = new Student();
+        $form = $this->createForm(StudentType::class, $student);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($student);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('student_list');
+        }
+
+        return $this->renderForm('student/create.html.twig', [
+            'student' => $student,
+            'form' => $form,
+        ]);
+    }
+
 }
