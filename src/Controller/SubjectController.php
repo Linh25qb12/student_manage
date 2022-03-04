b35155d1a1b5f3df58de4655c7c291631200063a
MinhTri24 <89444515+MinhTri24@users.noreply.github.com>
2022-03-04 11:03:46 +0700

    Create Subject

--- src/Controller/SubjectController.php
+++ src/Controller/SubjectController.php
@@ -3,9 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Subject;
+use App\Form\SubjectType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class SubjectController extends AbstractController
 {
@@ -19,5 +22,28 @@ class SubjectController extends AbstractController
         return $this->render('subject/index.html.twig',
             array('subjects' => $subjects,)
         );
+    }
+
+    /**
+     * @Route("/subject/create", name="subject_create", methods={"GET","POST"})
+     */
+    public function subjectCreate(Request $request, EntityManagerInterface $entityManager) : Response
+    {
+        $subject = new Subject();
+        $form = $this->createForm(SubjectType::class, $subject);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $subject->setName($request->request->get('subject')['Name']);
+            $entityManager->persist($subject);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('list_subject');
+        }
+
+        return $this->renderForm('subject/create.html.twig', [
+            'category' => $subject,
+            'form' => $form,
+        ]);
     }
 }
